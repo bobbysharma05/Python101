1489eb874d4bcb65bf2ea0fa66ce2b6c07e66b62
bobbysharma05 <141633552+bobbysharma05@users.noreply.github.com>
2023-08-29 23:38:55 +0530

    Age Problems.py

--- Ass_ques2..py
+++ Ass_ques2..py
@@ -1,3 +1,9 @@
+"""The program should accept an unknown number of ages from the keyboard until
+zero is entered. If the age is greater than or equal to 100 output “You have lived
+a century”, otherwise if age is greater than or equal to 55 output “Great Age to
+Relax” otherwise output “Welcome aboard”."""
+
+
 x = int(input("Enter your age = "))
 if x>=100:
     print("You have lived a century")
