96f936ff1e28b0c368818d634d93247c46e80977
bobbysharma05 <141633552+bobbysharma05@users.noreply.github.com>
2023-08-29 23:34:10 +0530

    Add files via upload

---
+++ Ass_ques2..py
@@ -0,0 +1,9 @@
+x = int(input("Enter your age = "))
+if x>=100:
+    print("You have lived a century")
+elif x>=55:
+    print("Great age to relax") 
+elif x==0:
+    print("Invalid input")
+else:
+    print("Welcome Abroad")       
---
+++ Ass_ques4.py
@@ -0,0 +1,11 @@
+x = int(input("Enter the number = "))
+
+i = 2
+c = x%i 
+i = i+1
+if x!=i and c!=0:
+    print("It is a prime number")
+else:
+    print("It is not a prime number")
+
+   
