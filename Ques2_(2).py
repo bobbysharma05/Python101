e6362a4e1976fa08510aee04e974427105813a66
bobbysharma05 <141633552+bobbysharma05@users.noreply.github.com>
2023-09-24 21:01:31 +0530

    Ques2_(2).py

--- Ques2_(2).py
+++ Ques2_(2).py
@@ -1,3 +1,8 @@
+''' Find the sum of the following series for N terms, where N is taken as input from the user and 
+display the result till exactly 9 decimal places.
+1/1! + 1/2! + 1/3! + 1/4! + … + 1/N!'''
+
+
 terms = int(input("Enter the number of terms = "))
 sum=0
 fact=1
