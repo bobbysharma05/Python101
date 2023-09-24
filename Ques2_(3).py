f3a14b8193793385f7f5cd424a34f175df5dfa34
bobbysharma05 <141633552+bobbysharma05@users.noreply.github.com>
2023-09-24 21:02:26 +0530

    Ques2_(3).py

--- Ques2_(3).py
+++ Ques2_(3).py
@@ -1,3 +1,13 @@
+''' Find the sum of the following series for N terms, where N is taken as input from the user and 
+display the result till exactly 9 decimal places.
+
+ F(x) = 1 + x/1 + x2
+/2 + x3
+/3 + x4
+/4 + ….. + N terms (Take x also as input)'''
+
+
+
 terms = int(input("Enter the number of terms = "))
 x= float(input("Enter the value of x"))
 sum=1
