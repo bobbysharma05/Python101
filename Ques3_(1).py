0840fcc31bbc8ae1bb75ce7710c7719e93655123
bobbysharma05 <141633552+bobbysharma05@users.noreply.github.com>
2023-09-24 21:03:08 +0530

    Ques3_(1).py

--- Ques3_(1).py
+++ Ques3_(1).py
@@ -1,3 +1,11 @@
+'''Find the sum of the series where x and a positive error ϵ is taken as input from the user. The 
+value of F(x) is displayed along with the minimum number of terms that gives the valid result 
+such that 
+ | Fi(x) – Fi-1(x) | < ϵ where i is the term count
+1. F = Σn ( 1 / 2 )n where n is the term count'''
+
+
+
 error = float(input("Enter the error : "))
 if error<0:
     print("Invalid Input") 
